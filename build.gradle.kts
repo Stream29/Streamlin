4559bd7fb4564da6e2405056a8c41e4a0aa7992b
Stream <1542763342@qq.com>
2024-09-19 03:25:19 +0800

    feat: init

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "io.stream29"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
