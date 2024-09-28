f10a465c3cd13403894da6dada5e1da0fe105f4b
Stream <1542763342@qq.com>
2024-09-28 20:40:00 +0800

    docs: KDoc for PrettyPrint

--- src/main/kotlin/io/github/stream29/streamlin/PrettyPrint.kt
+++ src/main/kotlin/io/github/stream29/streamlin/PrettyPrint.kt
@@ -1,17 +1,46 @@
 package io.github.stream29.streamlin
 
+/**
+ * Pretty print a string presentation of an object and end the line.
+ *
+ * Increase indentation for nested structures.
+ *
+ * @param any The object to pretty print.
+ */
 fun prettyPrintln(any: Any?) {
     prettyPrint(any)
     println()
 }
 
+/**
+ * Pretty print a string presentation of an object.
+ *
+ * Increase indentation for nested structures.
+ *
+ * @param any The object to pretty print.
+ */
 fun prettyPrint(any: Any?) = prettyPrint(any.toString())
+
+/**
+ * Pretty print a string and end the line.
+ *
+ * Increase indentation for nested structures.
+ *
+ * @param text The string to pretty print.
+ */
 fun prettyPrint(text: String) = print(text.toPrettyFormat())
 
 private val leftBracket = setOf('{', '[', '(')
 private val rightBracket = setOf('}', ']', ')')
 private val blank = setOf(' ', '\t', '\n')
 
+/**
+ * Pretty format a string.
+ *
+ * Increase indentation for nested structures.
+ *
+ * @return The pretty formatted string.
+ */
 fun String.toPrettyFormat(): String {
     val indent = " "
     val buffer = StringBuilder()
