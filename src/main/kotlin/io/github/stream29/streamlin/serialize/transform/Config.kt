8ffe344e8dee445ffc7532fe8f7d2b9c71e7632b
Stream <1542763342@qq.com>
2024-09-28 20:44:54 +0800

    docs: KDoc for transform/Config

--- src/main/kotlin/io/github/stream29/streamlin/serialize/transform/Config.kt
+++ src/main/kotlin/io/github/stream29/streamlin/serialize/transform/Config.kt
@@ -1,5 +1,11 @@
 package io.github.stream29.streamlin.serialize.transform
 
+/**
+ * The configuration to use for encoding and decoding.
+ *
+ * @property encodeDefault Whether to encode default values.
+ * @property encodeNull Whether to encode null values.
+ */
 data class TransformConfiguration(
     var encodeDefault: Boolean = true,
     var encodeNull: Boolean = false
