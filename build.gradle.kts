dc34f8e0d4bfa4fb2317415b72d4cadd939241b5
Stream <1542763342@qq.com>
2025-01-21 14:12:30 +0800

    build: add more targets and set jvmToolChain to 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JsModuleKind
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+import org.jetbrains.kotlin.konan.target.HostManager
 import java.net.URI
 
 plugins {
@@ -15,8 +19,67 @@ repositories {
 }
 
 kotlin {
-    jvm()
-    js().browser()
+    explicitApi()
+    jvm {
+        withJava()
+    }
+    jvmToolchain(8)
+
+    js {
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "10s"
+                }
+            }
+        }
+
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            sourceMap = true
+            moduleKind = JsModuleKind.MODULE_UMD
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        nodejs()
+    }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmWasi {
+        nodejs()
+    }
+
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    applyDefaultHierarchyTemplate {
+
+    }
+    if (HostManager.hostIsMac) {
+        // According to https://kotlinlang.org/docs/native-target-support.html
+        // Tier 1
+        macosX64()
+        macosArm64()
+        iosSimulatorArm64()
+        iosX64()
+        // Tier 2
+        watchosSimulatorArm64()
+        watchosX64()
+        watchosArm32()
+        watchosArm64()
+        tvosSimulatorArm64()
+        tvosX64()
+        tvosArm64()
+        iosArm64()
+        // Tier 3
+        watchosDeviceArm64()
+    }
+
+    // Tier 2
+    linuxX64()
+    linuxArm64()
+
+    // Tier 3
+    mingwX64()
 
     sourceSets {
         val commonMain by getting {
