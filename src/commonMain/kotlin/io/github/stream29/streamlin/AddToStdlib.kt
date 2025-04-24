96ee2286c7ce485f78cafce376249200f82673ce
Stream <1542763342@qq.com>
2025-04-25 00:36:07 +0800

    refactor: remove `filter` extension because it may clash with `filter` of `Iterable`

--- src/commonMain/kotlin/io/github/stream29/streamlin/AddToStdlib.kt
+++ src/commonMain/kotlin/io/github/stream29/streamlin/AddToStdlib.kt
@@ -1,26 +1,5 @@
 package io.github.stream29.streamlin
 
-import kotlin.jvm.JvmName
-
-/**
- * Filters a value based on a predicate.
- *
- * @param predicate A function that returns true if the value should be kept, false otherwise.
- * @return The value if the predicate returns true, null otherwise.
- */
-public inline fun <T : Any> T.filter(crossinline predicate: T.() -> Boolean) =
-    if (predicate()) this else null
-
-/**
- * Filters a value based on a predicate.
- *
- * @param predicate A function that returns true if the value should be kept, false otherwise.
- * @return The value if the predicate returns true, null otherwise.
- */
-@JvmName("filterNullable")
-public inline fun <T : Any> T?.filter(crossinline predicate: T.() -> Boolean) =
-    if (this != null && predicate()) this else null
-
 /**
  * Casts the receiver to the specified type.
  *
